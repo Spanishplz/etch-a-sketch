2f48cbaed563ddadc245f52da4559a4737bcaeaa
spanishplz <spanishplz@gmail.com>
2026-09-02 09:53:19 -0500

    Add screen layout

--- script.js
+++ script.js
@@ -4,27 +4,42 @@ const screenDiv = document.querySelector("#screenDiv");
 const screen = document.querySelector("#screen");
 
 // makes it a square
-screen.style.width = `${screen.offsetHeight}px`;
-
+screen.style.width = `${screen.clientHeight}px`;
+console.log(screen.offsetHeight);
+console.log(screen.offsetWidth);
 // create pixel divs
-const pixel = document.createElement("div");
-pixel.setAttribute("style","border: 1px solid deepskyblue; min-height: 20px; min-width: 20px; flex: 0 0 auto;");
-screen.appendChild(pixel);
+
+function createPixel(side) {
+    const pixel = document.createElement("div");
+    pixel.setAttribute("style","border: 1px solid deepskyblue; flex: 0 0 auto; ");
+    // pixel.style.boxSizing = "content-box";
+    // pixel.setAttribute("style","background-color: green; flex: 0 0 auto;");
+
+    pixel.style.height = `${side}px`;
+    pixel.style.width = `${side}px`;
+        screen.appendChild(pixel);
+    // console.log(pixel.offsetHeight);
+
+}
+
 
 // values of the screen size
-let screenH = screen.offsetHeight; // 500
-let screenW = screen.offsetWidth; // 500
+let screenH = screen.clientHeight; // 500
+let screenW = screen.clientWidth; // 500
 
 // values of each square "pixel" size
-let pixelH = pixel.offsetHeight;
-let pixelW = pixel.offsetWidth;
+// let pixelH = pixel.clientHeight;
+// let pixelW = pixel.clientWidth;
 
 // input
 let squares = 16;
-
+let total = squares*squares;
 let pixelSize = screenW/squares;
-console.log(pixelSize);
 
-for(let i =  0; i < squares; i++) {
-    
+
+for(let i =  0; i < total; i++) {
+    // console.log(i);
+    createPixel(pixelSize);
 };
+
+console.log(pixelSize * 16 );
